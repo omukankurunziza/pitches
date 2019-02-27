d316b5266c95e11f56763730fe1e138101217beb
mukankunziza opportunee <nshutioppo@yahoo.fr>
2019-02-28 00:43:48 +0200

    add columns in pitch class

--- app/models.py
+++ app/models.py
@@ -2,52 +2,8 @@ from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from . import login_manager
+# from datetime import datetime
 
-
-# class Movie:
-#     '''
-#     Movie class to define Movie Objects
-#     '''
-
-#     def __init__(self,id,title,overview,poster,vote_average,vote_count):
-#         self.id =id
-#         self.title = title
-#         self.overview = overview
-#         self.poster = "https://image.tmdb.org/t/p/w500/" + poster
-#         self.vote_average = vote_average
-#         self.vote_count = vote_count
-
-
-
-# class Review:
-
-#     all_reviews = []
-
-#     def __init__(self,movie_id,title,imageurl,review):
-#         self.movie_id = movie_id
-#         self.title = title
-#         self.imageurl = imageurl
-#         self.review = review
-
-
-#     def save_review(self):
-#         Review.all_reviews.append(self)
-
-
-#     @classmethod
-#     def clear_reviews(cls):
-#         Review.all_reviews.clear()
-
-#     @classmethod
-#     def get_reviews(cls,id):
-
-#         response = []
-
-#         for review in cls.all_reviews:
-#             if review.movie_id == id:
-#                 response.append(review)
-
-#         return response
 
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
@@ -55,7 +11,6 @@ class User(UserMixin,db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255),index = True)
     email = db.Column(db.String(255),unique = True,index = True)
-    
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     pass_secure = db.Column(db.String(255))
@@ -84,8 +39,11 @@ class Pitch(db.Model):
     __tablename__ = 'pitches'
 
     id = db.Column(db.Integer,primary_key = True)
+    title =  db.Column(db.String(255)) 
     description = db.Column(db.String(255))
     category = db.Column(db.String(255))
+    upvote = db.Column(db.Integer)
+    downvote = db.Column(db.Integer)
     user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
     comments = db.relationship('Comment',backref = 'pitch',lazy="dynamic")
     
