f992e1784b5f6ba4852ee8a25dcca18c23f117b4
mukankunziza opportunee <nshutioppo@yahoo.fr>
2019-02-28 00:55:57 +0200

    add forms.py

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,14 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField,ValidationError
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
 from wtforms.validators import Required,Email
-from ..models import User
+# from ..models import User
 
-class ReviewForm(FlaskForm):
-
-    title = StringField('Review title',validators=[Required()])
-    review = TextAreaField('Movie review', validators=[Required()])
+class PitchForm(FlaskForm):
+    title= StringField('Pitch Title', validators=[Required()])
+    pitch_description = TextAreaField('Write Pitch', validators=[Required()])  
+    category = SelectField('Pick Category', choices=[('Pickup Lines', 'Pickup Lines'), ('Interview Pitch', 'Interview Pitch'), ('Product Pitch', 'Product Pitch'), ('Promotion Pitch', 'Promotion Pitch')], validators=[Required()])  
     submit = SubmitField('Submit')
 
 class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    bio = TextAreaField('Say something about yourself...', validators = [Required()])
+    submit = SubmitField('Submit')
+
+class CommentsForm(FlaskForm):
+    description = TextAreaField('Write a comment...', validators=[Required()])
     submit = SubmitField('Submit')
